7df7d9e8c3b3d16cf95e23f0ec75d6b7ed585203
Adam Vandenberg <flangy@gmail.com>
2009-02-21 22:23:28 -0800

    Another test.

--- lib/tests/blocks.py
+++ lib/tests/blocks.py
@@ -3,8 +3,6 @@ from __future__ import with_statement
 import unittest
 from c64 import blocks
 
-# def blocks(bytes, block_size, offset=0, max=0):
-
 class BlockTests(unittest.TestCase):
     def test_blocks(self):
         b = list (blocks('abcdefgh', 2))
@@ -17,6 +15,10 @@ class BlockTests(unittest.TestCase):
     def test_offset(self):
         b = list (blocks('abcdefgh', 2, offset=2))
         self.assertEquals(3, len(b))
+        
+    def test_max_offset(self):
+        b = list (blocks('abcdefgh', 3, max=2, offset=2))
+        self.assertEquals(2, len(b))
         
 if __name__ == "__main__":
     unittest.main()  
