4a58da42cad9caf084a0db4ca07e17200d474d5b
Adam Vandenberg <flangy@gmail.com>
2009-03-02 23:28:48 -0800

    Style fix.

--- lib/tests/blocks.py
+++ lib/tests/blocks.py
@@ -5,19 +5,19 @@ from c64 import blocks
 
 class BlockTests(unittest.TestCase):
     def test_blocks(self):
-        b = list (blocks('abcdefgh', 2))
+        b = list(blocks('abcdefgh', 2))
         self.assertEquals(4, len(b))
         
     def test_max(self):
-        b = list (blocks('abcdefgh', 2, max=2))
+        b = list(blocks('abcdefgh', 2, max=2))
         self.assertEquals(2, len(b))
         
     def test_offset(self):
-        b = list (blocks('abcdefgh', 2, offset=2))
+        b = list(blocks('abcdefgh', 2, offset=2))
         self.assertEquals(3, len(b))
         
     def test_max_offset(self):
-        b = list (blocks('abcdefgh', 3, max=2, offset=2))
+        b = list(blocks('abcdefgh', 3, max=2, offset=2))
         self.assertEquals(2, len(b))
         
 if __name__ == "__main__":
