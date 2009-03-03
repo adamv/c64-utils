1dbc98c15b2f84deab5852941720a18ca550cd08
Adam Vandenberg <flangy@gmail.com>
2009-03-03 11:22:03 -0800

    Bootsectors should use 128 tables.

--- lib/boot.sh
+++ lib/boot.sh
@@ -1,2 +1,2 @@
 # Demonstrate disassembly of a bootsector.
-python -m gdis -i bootsector.prg --bootsector --data="[['C868', 5, 'Filename']]" --symbols kernal-table ports
+python -m gdis -i bootsector.prg --bootsector --data="[['0B68', 5, 'Filename']]" --symbols kernal-128 ports
