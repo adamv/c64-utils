b13de3654235466c4bd5f190b59297bb91d92c9e
Adam Vandenberg <flangy@gmail.com>
2009-03-03 19:21:43 -0800

    Experimenting with code coverage via coverage.py: http://nedbatchelder.com/code/modules/coverage.html

---
+++ lib/cover.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import coverage
+import unittest
+import tests
+
+coverage.erase()
+coverage.start()
+
+import c64
+import c64.bytestream
+import c64.formats
+import c64.formats.basic
+import c64.formats.bootsector
+import c64.formats.d64
+import c64.formats.petscii
+import c64.formats.prg
+import c64.formats.t64
+
+suite = unittest.TestSuite()
+suite.addTest(unittest.defaultTestLoader.loadTestsFromNames(["tests"]))
+
+testrunner = unittest.TextTestRunner()
+testrunner.run(suite)
+
+coverage.stop()
+coverage.report([
+    c64,
+    c64.bytestream,
+    c64.formats,
+    c64.formats.basic,
+    c64.formats.bootsector,
+    c64.formats.d64,
+    c64.formats.petscii,
+    c64.formats.prg,
+    c64.formats.t64,
+    ])
