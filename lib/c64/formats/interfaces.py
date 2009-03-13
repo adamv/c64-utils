96a6079c786688b6b03cb0cfadbf8c0b869391fe
Adam Vandenberg <flangy@gmail.com>
2009-03-12 23:51:56 -0700

    Informal file/container interfaces.

---
+++ lib/c64/formats/interfaces.py
@@ -0,0 +1,69 @@
+"""This module defines the abstract 'file container' interfaces."""
+
+class FileContaner(object):
+    """Represents a container for files or program data.
+    
+    A container has a name and a list of entries, at a minimum, as well
+    as format-specific methods and properties.
+    """
+    
+    @property
+    def entries(self):
+        """A list of entries in this container."""
+        return list()
+    
+    def file(self, i):
+        """Return a string containing file bytes for entry at index i.
+        
+        Some containers may only wrap a single file, in this case
+        the file has index 0.
+        """
+        raise FileNotFoundError
+        
+    def find(self, filename, ignore_case=False):
+        """Return a string containing file bytes for the named entry.
+        
+        Some containers may only wrap a single file, in this case
+        the name of that file is container-specfic, and should not
+        be relied upon.
+        
+        Filenames are case-sensitive, unless `ignore_case` is 
+        passed as `True`.
+        """
+        raise FileNotFoundError
+
+
+# Entry formats are container-specific, as the entry will have properties
+# needed by the container to be able to retreive the bytes for that entry.
+# (For instance, a disk entry will contain the track/sector information.)
+#
+# The common properties are listed here.
+
+class FileEntry(object):
+    """Represents a single entry in a container, or a wrapped file.
+    
+    Entries may not know how to retreive their own data bytes.
+    Typically the container is needed to be able to extract the bytes
+    for a single entry.
+    """
+    
+    @property
+    def raw_name(self):
+        """Name of this entry, as raw bytes from the container format.
+        
+        The `raw_name` of an entry is the actual bytes of the filename
+        per the container file-format. This may include trailing spaces,
+        trailing shift-spaces, or other format-specific bytes.
+        
+        Use `name` for a host-printable name.
+        """
+        pass
+    
+    @property
+    def name(self):
+        """Display name of this entry.
+        
+        The display name is based on the raw_name, but will have
+        trailing spaces and shift-spaces stripped off.
+        """
+        pass
